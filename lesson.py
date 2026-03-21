76721867f4bf889695a3c93e7ce1db6e718bc005
Ivan <irudenko.kiev@gmail.com>
2026-03-21 11:33:35 +0200

    Random + multi check foe win

--- lesson.py
+++ lesson.py
@@ -1,3 +1,5 @@
+import random
+
 a = int(input())
 
 matrix = [[" " for _ in range(a)] for _ in range(a)]
@@ -5,14 +7,48 @@ win = False
 current_player = "x"
 
 while True:
-    h = int(input())
-    w = int(input())
-    matrix[h-1][w-1] = current_player
-    print(matrix)
+    if current_player == "x":
+        h = int(input())
+        w = int(input())
+
+    else:
+        h = random.randint(1, a)
+        w = random.randint(1, a)
+
+    if matrix[h - 1][w - 1] == " ":
+        matrix[h-1][w-1] = current_player
+
+        for col in matrix:
+            print(col)
+    else:
+        print("Invalid move!")
+        continue
+
     for i in range(a):
         if matrix[h-1][i] != current_player:
             win = False
             break
+        else:
+            win = True
+    if not win:
+        for i in range(a):
+            if matrix[i][w - 1] != current_player:
+                win = False
+                break
+            else:
+                win = True
+    if not win:
+        for i in range(a):
+            if matrix[i][i] != current_player:
+                win = False
+                break
+            else:
+                win = True
+    if not win:
+        for i in range(a):
+            if matrix[i][a - i - 1] != current_player:
+                win = False
+                break
         else:
             win = True
 
