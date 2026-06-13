395e0899d1389f9e19d44cc620bc66e98614b4d7
Ivan <irudenko.kiev@gmail.com>
2026-06-13 11:26:47 +0300

    second_lesson

--- interactive.py
+++ interactive.py
@@ -1,9 +1,11 @@
 import uuid
 import datetime
-
+import logging
 
 my_trabsactions = {}
 curency_rate = {"USD":43,"UAH":1,"EUR":52}
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def add_transaction(category, amount, currency):
@@ -19,7 +21,12 @@ add_transaction("Films",100,"USD")
 print(my_trabsactions)
 
 def edit_transaction(id_trans, **kwargs):
-    transaction = my_trabsactions[id_trans]
+    try:
+        transaction = my_trabsactions[id_trans]
+    except KeyError:
+        print("Немає такої транзакції")
+        return
+
     for key, value in kwargs.items():
         if key in transaction:
             transaction[key] = value
@@ -31,21 +38,68 @@ edit_transaction(my_id, category = "Education")
 print(my_trabsactions)
 
 def delete_transaction(id_trans):
-    if id_trans not in my_trabsactions:
-        print("Друже, нема таої транзакції")
-    else:
+    try:
         del my_trabsactions[id_trans]
+
+    except KeyError:
+        print("Нема такого ID")
+        logging.warning("Невірне введення транзакції")
+
 
 delete_transaction(my_id)
 print(my_trabsactions)
 
 def curensy_translate_uah(curency,amount):
-    if curency not in curency_rate:
-        print("З такою валютою не працюємо")
-        return None
-    else:
+    try:
         final_amount_uah = amount * curency_rate[curency]
         return final_amount_uah
+    except KeyError:
+        print("З такою валютою не працюємо")
+        return 0
 
-print(curensy_translate_uah("USD",100))
+print(curensy_translate_uah("POL",100))
 
+def get_transaction_in_range(date_from, date_to):
+    result = {}
+    for key, value in my_trabsactions.items():
+        trans_date = datetime.datetime.fromisoformat(value["date"])
+        if date_from <= trans_date <= date_to:
+            result[key] = value
+    return result
+
+def zvit(dict_transaction):
+    total = 0
+    for key, value in dict_transaction.items():
+        amount_in_uah = curensy_translate_uah(value["currency"], value["amount"])
+        total += amount_in_uah
+    print(f"Потрачено за введений період: {total}")
+
+def month(year = None,month = None):
+    now = datetime.datetime.now()
+    year_data = now.year if year is None else year
+    month_data = now.month if month is None else month
+    if month_data  == 12:
+        date_to = datetime.datetime(year_data+1, 1, 1)
+    else:
+        date_to = datetime.datetime(year_data, month_data + 1, 1)
+    date_from = datetime.datetime(year_data,month_data,1)
+    range_of_transactions = get_transaction_in_range(date_from, date_to)
+    zvit(range_of_transactions)
+
+def week():
+    now = datetime.datetime.now()
+    date_from = now - datetime.timedelta(weeks=1)
+    range_of_transactions = get_transaction_in_range(date_from, now)
+    zvit(range_of_transactions)
+
+def year(year = None):
+    now = datetime.datetime.now()
+    year_data = now.year if year is None else year
+    date_from = datetime.datetime(year_data, 1, 1)
+    date_to = datetime.datetime(year_data + 1, 1, 1)
+    range_of_transactions = get_transaction_in_range(date_from, date_to)
+    zvit(range_of_transactions)
+
+month()
+week()
+month(2026,6)
